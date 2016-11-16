6fac1609b72d2c37a13219b46f13713ae99fe830
Oscar <osanfelix@outlook.es>
2016-11-16 18:07:31 +0100

    Proyecto vacío

---
+++ app/src/main/java/com/example/oscar/cookbook/CookbookActivity.java
@@ -0,0 +1,20 @@
+package com.example.oscar.cookbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CookbookActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cookbook);
+    }
+
+    public void onClick(View view)
+    {
+
+    }
+}
